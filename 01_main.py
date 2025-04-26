75b484da92562fbd610b1841b1fc1fdba1c8a033
aaban-khan <aabankhan.404@gmail.com>
2025-04-26 23:09:20 +0530

    first commit

---
+++ 01_main.py
@@ -0,0 +1,19 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+while True:
+    speak = str(input("🤖 What you Want me to Say? "))
+    if speak.lower() == "q":
+        break
+    engine.say(speak)
+    engine.runAndWait()
+
+
+# import pyttsx3
+
+# engine = pyttsx3.init()
+# voices = engine.getProperty('voices')
+
+# for index, voice in enumerate(voices):
+#     print(f"{index}: {voice.name} - {voice.id}")
